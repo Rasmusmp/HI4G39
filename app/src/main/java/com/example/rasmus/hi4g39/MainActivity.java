c10509b15e4bfddac18fb12ef5a757e28ccfa03b
Rasmus <rasmusmp@hotmail.com>
2015-05-09 13:02:09 +0200

    #change v.1.1 : ListFragment

--- app/src/main/java/com/example/rasmus/hi4g39/MainActivity.java
+++ app/src/main/java/com/example/rasmus/hi4g39/MainActivity.java
@@ -22,17 +22,20 @@ public class MainActivity extends FragmentActivity implements AdapterView.OnItem
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
         ListView seriesListView = (ListView) findViewById(R.id.series_listView);
         String[] series = getResources().getStringArray(R.array.series_array);
         seriesListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, series));
         seriesListView.setOnItemClickListener(this);
+        */
     }
+
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
-                Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getApplicationContext(), ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
     }
+
 
     @Override
     protected void onStart() {
--- app/src/main/java/com/example/rasmus/hi4g39/MenuFragment.java
+++ app/src/main/java/com/example/rasmus/hi4g39/MenuFragment.java
@@ -2,20 +2,31 @@ package com.example.rasmus.hi4g39;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
+import android.support.v4.app.ListFragment;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
  * Created by Rasmus on 09-05-2015.
  */
-public class MenuFragment extends Fragment{
+public class MenuFragment extends ListFragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        String[] series = getResources().getStringArray(R.array.series_array);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1, series);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
 
     }
 }
