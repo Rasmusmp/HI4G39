3b6ea4723ec715978c86756c84e417b9e3723ad6
Rasmus <rasmusmp@hotmail.com>
2015-05-09 12:39:46 +0200

    #change v.1.0

--- app/src/main/java/com/example/rasmus/hi4g39/MainActivity.java
+++ app/src/main/java/com/example/rasmus/hi4g39/MainActivity.java
@@ -1,41 +1,63 @@
 package com.example.rasmus.hi4g39;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-// i am making a change for GitHub
-// Thank you
 
-public class MainActivity extends ActionBarActivity {
+/*
+* listView ref: http://theopentutorials.com/tutorials/android/listview/android-create-listview-in-xml-and-populate-items-using-arrayadapter/
+*
+*/
+
+public class MainActivity extends FragmentActivity implements AdapterView.OnItemClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        ListView seriesListView = (ListView) findViewById(R.id.series_listView);
+        String[] series = getResources().getStringArray(R.array.series_array);
+        seriesListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, series));
+        seriesListView.setOnItemClickListener(this);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+                Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
 }
---
+++ app/src/main/java/com/example/rasmus/hi4g39/MenuFragment.java
@@ -0,0 +1,21 @@
+package com.example.rasmus.hi4g39;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by Rasmus on 09-05-2015.
+ */
+public class MenuFragment extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_menu, container, false);
+
+    }
+}
