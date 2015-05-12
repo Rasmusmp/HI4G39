4411d012862fcaa613a9f49f2fd865458d78a756
Nikolaj H <201205395@post.au.dk>
2015-05-12 13:15:18 +0200

    niko is in the house!

--- app/src/main/java/com/example/rasmus/hi4g39/ContentFragment.java
+++ app/src/main/java/com/example/rasmus/hi4g39/ContentFragment.java
@@ -1,6 +1,7 @@
 package com.example.rasmus.hi4g39;
 
 import android.app.Activity;
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,41 +11,49 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 
 public class ContentFragment extends Fragment implements SeriesSelectorInterface{
 
     String msg = "Rasmus Logging ";
-    View view;
+    TextView txtView;
     private String itemReceived;
+    private SeriesSelectorInterface seriesSelector;
+
+
 
     public ContentFragment() {
         // Required empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_content, container, false);
+        View view = inflater.inflate(R.layout.fragment_content, container, false);
 
-       // TextView itemTitle = (TextView) view.findViewById(R.id.content_title);
-        //itemTitle.setText(itemReceived);
 
-        TextView tvId = (TextView)view.findViewById(R.id.textViewA);
-        tvId.setText("Information about " + itemReceived);
+        txtView = (TextView) view.findViewById(R.id.textViewA);
+        updateInformation();
 
         return view;
+
     }
 
 
+    public void updateInformation() {
+
+        txtView.setText("blaaa     " + itemReceived);
+
+    }
+
+
+
     @Override
-    public void onItemReceived(String string) {
+    public void onItemReceived(String s) {
         //Do something with string received
-        itemReceived = string;
+        itemReceived = s;
         Log.d(msg, "ContentFrag: item: " + itemReceived);
-        TextView text=(TextView)view.findViewById(R.id.textViewA);
-        text.setText("Text");
     }
 
 
@@ -52,5 +61,21 @@ public class ContentFragment extends Fragment implements SeriesSelectorInterface
     public void onItemPicked(View view) {
 
     }
+
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        try{
+            seriesSelector = (SeriesSelectorInterface) activity;
+        } catch (ClassCastException ex) {
+            //Activity does not implement correct interface
+            throw new ClassCastException(activity.toString() + " must implement MovieSelectorInterface");
+        }
+
+    }
+
+
 
 }
